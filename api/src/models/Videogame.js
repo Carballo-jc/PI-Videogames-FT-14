8221142f92f29494414f88bb3702379e68d6ce7b
Jose Carballo <jczord@gmail.com>
2021-07-03 11:09:08 -0300

    modelos de datos de las tablas

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,24 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true,
     },
+    description: { type: DataTypes.TEXT, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false },
+    date_release: { type: DataTypes.DATE },
+    rating: { type: DataTypes.STRING },
+    platforms: { type: DataTypes.STRING, allowNull: false },
   });
+  // sequelize.define("gender", {
+  //   gameId: { type: DataTypes.INTEGER, unique: true },
+  //   name: { type: DataTypes.STRING },
+  // });
 };
